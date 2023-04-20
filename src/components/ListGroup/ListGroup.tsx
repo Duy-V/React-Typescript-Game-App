5ff840f5cd51dba0cde4ef7c2675e53439692922
tinhnt <tinhnt@skylab.vn>
2023-04-20 15:57:12 +0700

    css-in-js

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,24 +1,33 @@
 import { MouseEvent, useState } from "react";
 import styles from "./ListGroup.module.css";
+import styled from 'styled-components'
+
+const List = styled.ul`
+list-style: none;
+padding: 0;
+`
+interface ListItemProps {
+  active: boolean;
+}
+const ListItem = styled.li<ListItemProps>`
+padding: 5px 0;
+background: ${props => props.active ? 'blue' : 'none'}
+`
 interface Props {
   items: string[];
   heading: string;
   // onSelectItem: (item: string) => void;
 }
 export default function ListGroup({ items, heading}: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <div>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
-      <ul className={[styles.ListGroup, styles.container].join('')}>
+      <List>
         {items?.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            active = {index === selectedIndex}
             onClick={() => {
               setSelectedIndex(index);
               // onSelectItem(item);
@@ -26,9 +35,9 @@ export default function ListGroup({ items, heading}: Props) {
             key={item}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 }
