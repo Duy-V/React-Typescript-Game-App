b487237cc2578bec3dd206d56078adc02954f0f9
tinhnt <tinhnt@skylab.vn>
2023-04-20 13:22:45 +0700

    Vanilla CSS

---
+++ src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,21 @@
+import {MouseEvent, useState} from "react";
+import './ListGroup.css'
+interface Props {
+  items: string [];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+export default function ListGroup({items, heading,onSelectItem}: Props) {
+const [selectedIndex, setSelectedIndex] = useState(-1)  
+return (
+    <div>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p> }
+      <ul className="list-group">
+        {items?.map((item, index) => (
+          <li className= {selectedIndex === index ? "list-group-item active" : "list-group-item"} onClick={()=> {setSelectedIndex(index); onSelectItem(item)}}  key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
